33e630ab75419ce6b61ebf0390793e666cf429a3
Prathamesh Davande <prathamesh.pd.2000@gmail.com>
2024-05-29 20:31:54 +0530

    Added code with Context API also

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,11 +1,14 @@
+import { useQuiz } from "../contexts/QuizContext";
 import Options from "./Options";
 
-function Questions({ question, answer, dispatch }) {
-  //console.log(question);
+function Questions() {
+  const { questions, index } = useQuiz();
+  const question = questions.at(index);
+
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} answer={answer} dispatch={dispatch} />
+      <Options question={question} />
     </div>
   );
 }
